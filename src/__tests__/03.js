d301b12ea37800d7e542ff12bca94714af077681
Santosh Marigowda <santosharakere@gmail.com>
2018-12-02 17:26:12 +0000

    feedback submitted

--- src/__tests__/03.js
+++ src/__tests__/03.js
@@ -27,10 +27,10 @@ test('Usage works', async () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=modern%20react&e=03&em=
+http://ws.kcd.im/?ws=modern%20react&e=03&em=santosharakere@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
