2bd38671b7f2fe272896ca088a600b9e324d98a5
Santosh Marigowda <santosharakere@gmail.com>
2018-12-02 17:20:07 +0000

    useEffect exercise is complete

--- src/exercises/03.js
+++ src/exercises/03.js
@@ -1,17 +1,20 @@
 // Counter: useEffect
 // 🐨 2. you'll also want useEffect here!
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 // We moved things back to within the Counter component for the exercise.
 
 function Counter() {
   // 🐨 1. initialize the state to the value from localStorage
   // 💰 Number(window.localStorage.getItem('count') || 0)
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(Number(window.localStorage.getItem('count') || 0))
   const incrementCount = () => setCount(count + 1)
   // 3. 🐨 Here's where you'll use `useEffect`.
   // The callback should set the `count` in localStorage.
-  return <button onClick={incrementCount}>{count}</button>
+  useEffect(() => {
+    window.localStorage.setItem('count', count);
+  })
+  return <button onClick={ incrementCount }>{ count }</button>
 }
 
 // Don't make changes to the Usage component. It's here to show you how your
