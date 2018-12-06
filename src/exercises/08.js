4fee2143bd3c4f6376dfdf660a725404fb55e5a0
Santosh Marigowda <santosharakere@gmail.com>
2018-12-06 05:55:50 +0000

    exercise 08 complete

--- src/exercises/08.js
+++ src/exercises/08.js
@@ -18,32 +18,36 @@ const buttonStyles = {
 // console.log({...obj1, ...obj2}) // {a: 'b', c: 'e', f: 'g'}
 // 💰 when done correctly, this function should be no longer than 3 lines.
 // and could be done in one line if you make it an arrow function.
-function reducer(state, action) {
-  switch (action.type) {
-    case 'LAPSE':
-      return {
-        ...state,
-        lapse: action.now - action.startTime,
-      }
-    case 'TOGGLE_RUNNING':
-      return {
-        ...state,
-        running: !state.running,
-      }
-    case 'CLEAR':
-      return {
-        ...state,
-        running: false,
-        lapse: 0,
-      }
-    default:
-      break
-  }
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'LAPSE':
+//       return {
+//         ...state,
+//         lapse: action.now - action.startTime,
+//       }
+//     case 'TOGGLE_RUNNING':
+//       return {
+//         ...state,
+//         running: !state.running,
+//       }
+//     case 'CLEAR':
+//       return {
+//         ...state,
+//         running: false,
+//         lapse: 0,
+//       }
+//     default:
+//       break
+//   }
+// }
+
+function reducer(currentState, newState) {
+  return { ...currentState, ...newState }
 }
 
 function Stopwatch() {
   // 🐨 2. rename `dispatch` to `setState`
-  const [{running, lapse}, dispatch] = useReducer(reducer, {
+  const [{running, lapse}, setState] = useReducer(reducer, {
     running: false,
     lapse: 0,
   })
@@ -58,17 +62,20 @@ function Stopwatch() {
       const startTime = Date.now() - lapse
       timerRef.current = setInterval(() => {
         // 🐨 3. call `setState` instead
-        dispatch({type: 'LAPSE', now: Date.now(), startTime})
+        setState({ lapse: Date.now() - startTime })
+        // dispatch({type: 'LAPSE', now: Date.now(), startTime})
       }, 0)
     }
     // 🐨 4. call `setState` instead
-    dispatch({type: 'TOGGLE_RUNNING'})
+    setState({ running: !running });
+    // dispatch({type: 'TOGGLE_RUNNING'})
   }
 
   function handleClearClick() {
     clearInterval(timerRef.current)
     // 🐨 5. call `setState` instead
-    dispatch({type: 'CLEAR'})
+    setState({ running: false, lapse: 0 });
+    // dispatch({type: 'CLEAR'})
   }
 
   return (
